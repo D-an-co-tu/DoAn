aa134665fac532a10af0cda40d4fe6a4208bd198
Ngô Chí Cường <22dh110459@st.huflit.edu.vn>
2023-11-10 16:17:27 +0700

    Add project files.

---
+++ DoAn/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAn.Models;
+
+namespace DoAn.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        DBShopDunkEntities database = new DBShopDunkEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult SearchOption(double min = double.MinValue, double max = double.MaxValue)
+        {
+            var list = database.Products.Where(p => (double)p.Price >= min && (double)p.Price <= max).ToList();
+            return View(list);
+        }
+    }
+}
